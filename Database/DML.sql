f84aa3ab6e9560396f5228da153383f041d66c21
Line Guld <linekguld@hotmail.com>
2021-12-14 15:33:15 +0100

    DML update

--- Database/DML.sql
+++ Database/DML.sql
@@ -3,7 +3,10 @@ SET SCHEMA 'dnnr';
 INSERT INTO users (role, username, password)
 VALUES ('User', 'Bob', '5f4dcc3b5aa765d61d8327deb882cf99'),/*password*/
        ('User', 'Hans', '23a8cadbee3c6d52a8bdc33aa962f5be'),/*fisk*/
-       ('Admin', 'Admin', 'eb0a191797624dd3a48fa681d3061212'); /*master*/
+       ('Admin', 'Admin', 'eb0a191797624dd3a48fa681d3061212'), /*master*/
+       ('User', 'Superman', '6f7f9432d35dea629c8384dab312259a'), /*strong*/
+       ('User', 'Superwoman', '6f7f9432d35dea629c8384dab312259a'), /*strong*/
+       ('User', 'AllergyBoy', '657f8b8da628ef83cf69101b6817150a'); /*help*/
 
 INSERT INTO recipe (name)
 VALUES ('Kylling Korma'),
@@ -225,13 +228,80 @@ VALUES (6, 1),
        (19, 47);
 
 INSERT INTO foodgroupallergy (userid, foodgroupid)
-VALUES (1, 4);
+VALUES (1, 4),
+       (6, 1),
+       (6, 2),
+       (6, 3),
+       (6, 4),
+       (6, 5),
+       (6, 6),
+       (6, 7),
+       (6, 8),
+       (6,9),
+       (6,10),
+       (6,11),
+       (6,12),
+       (6, 13),
+       (6, 14),
+       (6, 15),
+       (6, 16),
+       (6, 17),
+       (6, 18),
+       (6, 19);
+
 
 INSERT INTO foodgroupaversion(userid, foodgroupid)
 VALUES (2, 18); --Religiøst bange for alkohol
 
-INSERT INTO ingredientallergy(userid, ingredientid, allergyname)
-VALUES (2, 9, 'Paprika-allergi');
+INSERT INTO ingredientallergy(userid, ingredientid)
+VALUES (2, 9),
+       (6, 1),
+       (6, 2),
+       (6, 3),
+       (6, 4),
+       (6, 5),
+       (6, 6),
+       (6, 7),
+       (6, 8),
+       (6,9),
+       (6,10),
+       (6,11),
+       (6,12),
+       (6, 13),
+       (6, 14),
+       (6, 15),
+       (6, 16),
+       (6, 17),
+       (6, 18),
+       (6, 19),
+       (6, 20),
+       (6, 21),
+       (6, 22),
+       (6, 23),
+       (6, 24),
+       (6, 25),
+       (6, 26),
+       (6, 27),
+       (6, 28),
+       (6,29),
+       (6,30),
+       (6,31),
+       (6,32),
+       (6, 33),
+       (6, 34),
+       (6, 35),
+       (6, 36),
+       (6, 37),
+       (6, 38),
+       (6, 39),
+       (6, 40),
+       (6, 41),
+       (6, 42),
+       (6, 43),
+       (6, 44),
+       (6, 45),
+       (6, 46),
+       (6, 47);
 
 INSERT INTO ingredientaversion(userid, ingredientid, preferencename)
 VALUES (1, 18, 'Højt blodtryk');
@@ -568,8 +638,11 @@ INSERT INTO users (id, role, username, password)
 VALUES (DEFAULT, 'Restaurateur', 'Lis', '7538ebc37ad0917853e044b9b42bd8a4') /*mad*/;
 
 INSERT INTO restaurant(cvr, name, theme, addressid, phonenumber, ownerid)
-VALUES (15, 'McDonalds', 'FastFood', 3, '+4512345678',4),
-       (94, 'Noma', 'Nordisk mad', 4, '+4587654321',4),
+VALUES (15, 'McDonalds', 'FastFood', 3, '+4512345678', 4),
+       (94, 'Noma', 'Nordisk mad', 4, '+4587654321', 4),
        (86, 'McNuggets', 'Kyllingebidder', 3, '+4570121416', 4);
 
-SELECT * FROM users WHERE role = 'Restaurateur' or role = 'User';
+SELECT *
+FROM users
+WHERE role = 'Restaurateur'
+   OR role = 'User';
